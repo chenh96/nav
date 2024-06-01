33ad2efa46b18f233157eee3c239e167a96698e4
Han Chen <chenh96@foxmail.com>
2024-06-01 23:37:51 +0800

    2024060102

--- nav-web/vite.config.ts
+++ nav-web/vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  publicDir: 'public',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: '导航',
+        short_name: '导航',
+        description: '导航',
+        lang: 'zh-cmn-Hans',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: './',
   server: {
     host: true,
--- nav/src/main/java/tech/chenh/nav/data/basic/BasicEntity.java
+++ nav/src/main/java/tech/chenh/nav/data/basic/BasicEntity.java
@@ -2,12 +2,14 @@ package tech.chenh.nav.data.basic;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
 public abstract class BasicEntity {
 
-    // @SequenceGenerator(name = "idGenerator")
-    // @GeneratedValue(generator = "idGenerator")
     @GeneratedValue
     @Id
     private long id;
